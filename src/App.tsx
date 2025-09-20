41b2414ba227ebee186b73f3745be630c87c9a6d
K-KY <kyuyoungk@naver.com>
2025-09-20 15:19:25 +0900

    Test : 로컬에서 영상 출력 테스트 : 성공

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import HlsPlayer from "./componentes/HlsPlayer.tsx";
 
 function App() {
 
   return (
     <>
+      <div style={{ padding: 16 }}>
+        <HlsPlayer
+            src="http://localhost:8080/prometheus-grafana/hls/output.m3u8" // nginx 등에서 서빙 중인 m3u8 경로
+            autoPlay
+            controls
+            muted
+        />
+      </div>
 
     </>
   )
