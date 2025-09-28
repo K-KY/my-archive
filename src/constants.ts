a6343ba91c8e7d7b7bbe29fd9d8456c3a4abe715
K-KY <kyuyoungk@naver.com>
2025-09-28 22:24:38 +0900

    Chore : 테스트용 API 주소 추가

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const BASE_URL = "https://kky.tail0a6d17.ts.net"
 export const API_URL = "https://kky.tail0a6d17.ts.net/api"
 export const HLS_URL = "https://kky.tail0a6d17.ts.net/hls"
-
+export const TEST_API_URL = "http://localhost:8080"
+// export const API_URL = TEST_API_URL
