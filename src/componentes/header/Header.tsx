7995144a3343fcb526338721cf736c8f19cf264c
K-KY <kyuyoungk@naver.com>
2025-10-02 00:27:35 +0900

    Feat : 임시 헤더

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className={"headerFrame"}>
+            <header className={"header"}>헤더</header>
+        </div>
+    )
+}
+
+export default Header;
