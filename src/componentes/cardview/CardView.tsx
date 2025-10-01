0bc90f87e1ddd64d0c64f77eda67565aa9228694
K-KY <kyuyoungk@naver.com>
2025-10-02 00:27:07 +0900

    Feat : 카드 뷰 컴포넌트

---
+++ src/componentes/cardview/CardView.tsx
@@ -0,0 +1,34 @@
+import "./CardView.css"
+
+
+interface CardViewProps {
+    title: string;
+    image: string;
+}
+const CardView = (prop:CardViewProps) => {
+    return (
+        <div className="card">
+            <div>
+                <ImageViewer img={prop.image}></ImageViewer>
+            </div>
+
+            <div className={"card-title"}>
+                <div>
+                    <p>{prop.title}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const ImageViewer = ({img}:{img:string}) => {
+    return (
+        <div className="card">
+            <div>
+                <img src={img} alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default CardView;
